9cf58fecb4f71d12d34371d9839947312a6b08c8
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2022-12-10 20:46:11 +0200

    little update

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const unread = document.querySelectorAll('.unread');
 const state = document.querySelectorAll('.status__state');
 
 mark.addEventListener('click',()=> {
-    popup.classList.add('none');
+    popup.classList.add('hidden');
     unread.forEach(item => {
         item.classList.remove('unread');
     })
-    state.forEach(item=> {
+    state.forEach(item => {
         item.classList.remove('status__state');
     })
 });
